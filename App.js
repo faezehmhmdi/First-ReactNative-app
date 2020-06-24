b5c10fc32af23d1f476ef377afcd8a9cacc7e740
faezehmhmdi <mohammadi_faezeh@aut.ac.ir>
2020-06-24 21:02:21 +0430

    first commit

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
-
+//first commit
 export default function App() {
   const [enteredGoal, setEnteredGoal] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
